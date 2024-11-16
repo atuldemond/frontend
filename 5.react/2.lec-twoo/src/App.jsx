28da49b3ed74de2c6521f30b85af88d531635a1f
atuldemond <atuldemond@gmail.com>
2024-11-16 13:00:40 +0530

    react revision started

---
+++ 5.react/2.lec-twoo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <h1>ATUL DEMOND</h1>
+      <h2>Revision then next REACT</h2>
+    </>
+  );
+};
+
+export default App;
