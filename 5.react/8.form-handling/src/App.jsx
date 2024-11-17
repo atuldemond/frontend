5989e9f2e4f419c37bd5044bd3a4b8cf7cc21f9f
atuldemond <atuldemond@gmail.com>
2024-11-17 21:09:10 +0530

    working on form handling   finish

---
+++ 5.react/8.form-handling/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <>
+      <Card name="ATUL Demond" age="24" />
+    </>
+  );
+};
+
+export default App;
